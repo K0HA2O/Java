2e6378673667caa7f4ef9905cc72dd3abf2b817f
K0HA2O <allsk6782@naver.com>
2025-04-20 23:55:53 +0900

    java-2025-04-20

--- src/ch6/CarTest.java
+++ src/ch6/CarTest.java
@@ -1,18 +1,18 @@
 package ch6;
 
-class Car {
-	String color; // 색상
-	String gearType; // 변속기 종류 - auto(자동), manual(수동)
-	int door; // 문의 개수
-	
-	Car() {	}
-	
-	Car (String c, String g, int d) {
-		color = c;
-		gearType = g;
-		door = d;
-	}
-}
+//class Car {
+//	String color; // 색상
+//	String gearType; // 변속기 종류 - auto(자동), manual(수동)
+//	int door; // 문의 개수
+//	
+//	Car() {	}
+//	
+//	Car (String c, String g, int d) {
+//		color = c;
+//		gearType = g;
+//		door = d;
+//	}
+//}
 
 public class CarTest {
 

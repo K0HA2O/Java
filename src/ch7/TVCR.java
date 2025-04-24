91559b090a72c8e0323e42d56f4c83f272c33eb5
K0HA2O <allsk6782@naver.com>
2025-04-24 19:33:44 +0900

    java-2025-04-24

--- src/ch7/TVCR.java
+++ src/ch7/TVCR.java
@@ -1,42 +1,42 @@
-package ch7;
-
-class Tv {
-	boolean power; // 전원상태(on/off)
-	int cannel; // 채널
-	
-	void power() { power = !power; }
-	void channelUp() { ++cannel; }
-	void cannelDown() { --cannel; }
-}
-
-class VCR {
-	boolean power; // 전원상태(on/off)
-	int counter = 0;
-	
-	void power() { power = !power; }
-	void play () {}
-	void stop() {}
-	void rew() {}
-	void ff() {}
-}
-
-class TVCR extends Tv {
-
-	VCR vcr = new VCR();
-	
-	void pay() {
-		vcr.play();
-	}
-	
-	void stop() {
-		vcr.stop();
-	}
-	
-	void rew() {
-		vcr.rew();
-	}
-	
-	void ff() {
-		vcr.ff();
-	}
-}
+//package ch7;
+//
+//class Tv {
+//	boolean power; // 전원상태(on/off)
+//	int cannel; // 채널
+//	
+//	void power() { power = !power; }
+//	void channelUp() { ++cannel; }
+//	void cannelDown() { --cannel; }
+//}
+//
+//class VCR {
+//	boolean power; // 전원상태(on/off)
+//	int counter = 0;
+//	
+//	void power() { power = !power; }
+//	void play () {}
+//	void stop() {}
+//	void rew() {}
+//	void ff() {}
+//}
+//
+//class TVCR extends Tv {
+//
+//	VCR vcr = new VCR();
+//	
+//	void pay() {
+//		vcr.play();
+//	}
+//	
+//	void stop() {
+//		vcr.stop();
+//	}
+//	
+//	void rew() {
+//		vcr.rew();
+//	}
+//	
+//	void ff() {
+//		vcr.ff();
+//	}
+//}
